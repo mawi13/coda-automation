f0235292deb953f18ad1962a342d9cac86b2c27d
mawi13 <42282372+mawi13@users.noreply.github.com>
2019-09-23 10:45:28 +0200

    Update pubkey_to_discord.py

--- scripts/testnet-tools/pubkey_to_discord.py
+++ scripts/testnet-tools/pubkey_to_discord.py
@@ -13,6 +13,7 @@ FILET_MIGNON_STAKING_CHALLENGE = {
     'tNciaKqn2vMY7JA66AHEP1bupBcsnPYeFM58SorCWL78pGoVKrM9r7j968a9BaqmEqH2hx8UCCURy8op27HFE9auLfesTbu4u7MmRQmsGVmWhMfXSqpTQpmkcjzW7hd4Bc44G32jv2SLLS': 'Matt Harrop / Figment Network',
     'tNciBBcEcajd4AFCAiSLWvwg9X3fCUT57X5h9UhVuDhh3UrZ26nxCeVEzjFCN6uVhy2CFSqUjWqGsZ2yQno64nqE6SWPbqVr1WX2JiBQ23mBCmtkViPxCQUdq2EJZPyMDx6tWbazW2xgYo': 'garethtdavies#4963'
     'tNciVRxhXhJq78LFxg8acp7VwVSXtJMsQgyyASr9q9vXMcJ4whwawrtz1TZyH8TXHyy68cPatDR5xRn5G2o7cm8kWYMbooPaTVpte2vYsd7pqFDAJ6QAYTD8Ro1FPmzQdvCZHdhdMfdZqi': 'novy',
+    'tNci2pAN18umqQ3Q3MJJqt7vGrYZAFE2pnatBoiZ1HTtf45YQ1tANhiRz5DLvR6Gcou682g1UWE2aTAHn2EbBYRmpg8Av1Nm8dc95b4ZaWMLNHvvJYaieK6MZzBfhnaNYjouwauTNLqFcM': 'masupilami',
 }
 
 # For Community Members
